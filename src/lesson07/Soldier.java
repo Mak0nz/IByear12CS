5c3fdb935214a73720dd44f91e51f36746bd3126
Middle and Senior <MsUser@seclab.local>
2023-01-11 11:42:56 +0100

    soldier work

---
+++ src/lesson07/Soldier.java
@@ -0,0 +1,44 @@
+package lesson07;
+
+public class Soldier {
+    
+    /**
+     * ------ Attributes ------
+     * These are all the properties that describe the state
+     */
+    int id;
+    int health;
+    String name;
+    String hungerLevel = "Hamborgor";
+    boolean isHungry;
+
+
+
+    /**
+     * ------ Coming soon ------
+     */
+
+
+
+    /**
+     * ------ Behavioral Method ------
+     * The functions the object can use to update the state.
+     */
+    public void damage() {
+        health = health - 1;
+    }
+
+    public void eat() {
+        if (isHungry) {
+            switch(hungerLevel) {
+                case "Hamborgor": {
+                    // todo give him 10 hamburg
+                }
+                case "shid": {
+                    isHungry = false;
+                    // todo give him 0 hamburg
+                }
+            }      
+        }
+    }
+}
