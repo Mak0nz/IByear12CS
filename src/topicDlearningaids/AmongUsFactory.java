04b0c344885b0f4ec76f27a487c57abf80d6841f
Middle and Senior <MsUser@seclab.local>
2023-01-25 11:37:05 +0100

    AmongUs randomized colour and role

--- src/topicDlearningaids/AmongUsFactory.java
+++ src/topicDlearningaids/AmongUsFactory.java
@@ -6,7 +6,18 @@ public class AmongUsFactory {
         // Example of using the "plain cookie cutter" constructor
         // What AmongUs colour do we get?
         AmongUs randomAstronaut = new AmongUs();
+        
+        AmongUs[] allCrew = new AmongUs[18];
          
         // TODO: Create an instances of the AmongUs in every colour
+        for(int c = 0; c < AmongUs.WARDROBE.length; c++) {
+            // create an instance of AmongUs in a colour
+            String role = AmongUs.ROLES[(c % 7)];
+            AmongUs astro = new AmongUs(AmongUs.WARDROBE[c], role);
+            // store it in allCrew
+            allCrew[c] = astro;
+        }
+
+        System.out.println("");
     }
 }
