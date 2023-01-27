3929984ad3261f21ff086d953553bdea20327a13
Middle and Senior <MsUser@seclab.local>
2023-01-27 10:04:34 +0100

    setrandomcolor setrandomrole + check availability for both

--- src/topicDlearningaids/AmongUs.java
+++ src/topicDlearningaids/AmongUs.java
@@ -3,12 +3,12 @@ package topicDlearningaids;
 import java.util.Random;
 
 /**
- * Define static
+ * D.3.3. Define static
  */
 public class AmongUs {
 
     /**
-     * - static means *no state* and does not 
+     * - static means *no state* and does not belong to one instance
      * - static attributes do not describe the state of an object instance
      * - static attributes can be accessed directly using the class
      */
@@ -23,15 +23,13 @@ public class AmongUs {
 
     // Declare a static final array with all the roles allowed
     public static final String[] ROLES = {
-        "Crew", "Imposter", "Ghost", "Scientist", "Engineer",
+        "Crew", "Ghost", "Scientist", "Engineer", "Imposter",
         "Shapeshifter", "Guardian Angel"
     };
 
     public static final int CREW_MAX = 10;
 
     public static final int IMPOSTER_MAX = 2;
-
-
 
     private static boolean[] AVAILABLE_COLOURS = {
         true, true, true,
@@ -83,16 +81,38 @@ public class AmongUs {
           // get a random number between 0 and WARDROBE.length - 1
           // https://stackoverflow.com/questions/2444019/how-do-i-generate-a-random-integer-between-min-and-max-in-java
           select = r.nextInt(WARDROBE.length - 1); 
-        } while(AVAILABLE_COLOURS[select] == false);
+        } while(!isColourAvailable(WARDROBE[select])); // 
         this.colour = WARDROBE[select];
         AVAILABLE_COLOURS[select] = false;
+    }
+
+    private boolean isColourAvailable(String colour) {
+        // perform linear search on colour to get index
+        for(int i = 0; i < WARDROBE.length; i++) {
+            if (WARDROBE[i].equals(colour)) {
+                return (AVAILABLE_COLOURS[i] == true);
+            }
+        }
+        return false;
     }
 
     /**
      * Gives this instance a random role
      */
     public void setRandmomRole() {
+        Random r = new Random();
+        int select = 0;
+        do {
+          // get a random number between 0 and ROLES.length - 1
+          // https://stackoverflow.com/questions/2444019/how-do-i-generate-a-random-integer-between-min-and-max-in-java
+          select = r.nextInt(ROLES.length - 1); 
+        } while(!isRoleAvailable(ROLES[select]));
+        this.role = ROLES[select];
+    }
 
+    private boolean isRoleAvailable(String role) {
+        // TODO: Build the rules for roles
+        return true;
     }
 
     /**
