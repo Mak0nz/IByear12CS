9e2a225910fa477550ebf4160dfce1815bf0bbd4
Middle and Senior <MsUser@seclab.local>
2023-03-01 10:00:56 +0100

    initialize pancake class + created isEmpty isFull

---
+++ src/topic05learningaids/stacks/PancakeMain.java
@@ -0,0 +1,9 @@
+package topic05learningaids.stacks;
+
+public class PancakeMain {
+    public static void main(String[] args) {
+        // instansiate a PancakeStack
+        PancakeStack myBreakfast = new PancakeStack("butter and Maple Syrup");
+
+    }
+}
