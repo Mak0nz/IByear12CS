5f4497901aae96ca06945d188b04451e619de4eb
Middle and Senior <benedek.praga@gmail.com>
2023-05-10 11:42:27 +0200

    dimitrii ia

---
+++ src/lesson08/dimitrii/Appointment.java
@@ -0,0 +1,24 @@
+package lesson08.dimitrii;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Appointment {
+    /* ------------- Attributes ------------- */
+    int id;
+    String title;
+    String type;
+    ArrayList<Guest> guests;
+    LocalDateTime date;
+    Location location;
+    Description description;
+    Calendar calendar;
+    ArrayList<Notification> notifications;
+
+
+    /* ------------- Constructor ------------- */
+
+
+    /* ------------- Behaviour ------------- */
+
+}
