303acf5407f58980793b35a423c2b511d0b34f0e
Middle and Senior <benedek.praga@gmail.com>
2023-06-14 09:26:07 +0200

    Nov 2017 HL p2 q6

---
+++ src/topicDlearningaids/nov2018hlp2/Nov2017hlp1q6.java
@@ -0,0 +1,13 @@
+package topicDlearningaids.nov2018hlp2;
+public class Nov2017hlp1q6 {
+    public static void main(String[] args) { 
+        int N;
+        N = 0;
+        
+        while (N>0 && N%2==0 ) {
+            N = N-2;
+        }
+        System.out.println(N);
+
+    }
+}
